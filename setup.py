48a4966396384a710763d1b1e14b68e222d44997
Abhishek Sawalkar <abhishek31sawalkar@gmail.com>
2024-07-29 16:07:35 -0400

    App running. Bundle having issue with libffi.8.dylib. Can't read arm64 arch file. Need x86_64

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup
+import os
+import shutil
 
-APP = ['counter_app.py']
-DATA_FILES = []
+# Paths to libffi and the source of your app
+LIBFFI_PATH = '/opt/homebrew/Cellar/libffi/3.4.6/lib'
+APP_PATH = 'counter_app.py'
+APP_NAME = 'counter_app'
+
+# Remove old libffi.dylib if it exists
+if os.path.exists(f'dist/{APP_NAME}.app/Contents/Frameworks/libffi.8.dylib'):
+    os.remove(f'dist/{APP_NAME}.app/Contents/Frameworks/libffi.8.dylib')
+
+# Setup.py configuration
+APP = [APP_PATH]
+DATA_FILES = [
+    ('Frameworks', [os.path.join(LIBFFI_PATH, 'libffi.dylib')])
+]
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['rumps'],
+    'plist': {
+        'LSUIElement': True,  # This makes the app run in the background without showing up in the dock
+    }
 }
 
 setup(
@@ -13,3 +30,23 @@ setup(
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
+
+
+# from setuptools import setup
+
+# APP = ['counter_app.py']
+# DATA_FILES = []
+# OPTIONS = {
+#     'argv_emulation': True,
+#     'packages': ['rumps'],
+#     'plist': {
+#         'LSUIElement': True,  # This makes the app run in the background without showing up in the dock
+#     }
+# }
+
+# setup(
+#     app=APP,
+#     data_files=DATA_FILES,
+#     options={'py2app': OPTIONS},
+#     setup_requires=['py2app'],
+# )
